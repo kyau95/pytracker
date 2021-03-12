8672700baed6929c66cb89976f62414cbe8ed1f2
Kevin <kyau97@gmail.com>
2021-03-11 20:58:35 -0500

    Added headers

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+File:       app.py
+Author:     ykevin1
+Info:       File contains the GUI information for the tracker program. Aims to
+            be user-friendly and intuitive to the user so that they can 
+            navigate the various screens easily and perform the tasks that they
+            want to accomplish with ease. 
+"""
+
+import tkinter as tk
+
+
+if __name__ == '__main__':
+
---
+++ tracker.py
@@ -0,0 +1,11 @@
+"""
+File:       tracker.py
+Author:     ykevin1
+Info:       This program is intended to be able to track products/items from a
+            website until this fall under a certain threshold price. The 
+            program will then notify the user when the item has met the target
+            so the user can consider the product for purchase.
+"""
+
+
+
